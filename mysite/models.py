820928b14ea582b1751966085adbba564fbe546d
gondan96 <qwertyden96@gmail.com>
2017-09-14 13:11:26 +0400

    First model
    набросок модели

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+#id по умолчанию(проверить)
+
+
+class People(models.Model):
+    name = models.CharField(max_length=40)
+    salary = models.IntegerField("Salary")
+    spend_now = models.IntegerField("Spend")
+
+
+class Eat(models.Model):
+    price = models.IntegerField()
+    image = models.ImageField()
+    #требует Pillow для пикчи
